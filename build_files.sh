1392c85f1f87191f087c2fdeeffdeacc00b51b0f
udontur <h@hadrian.cc>
2025-07-11 16:26:20 +0800

    fix: typo

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing & Building Dependencies... "
-python3 -m pip instgall -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files... "
 python3 manage.py collectstatic --noinput
