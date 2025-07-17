70d4c34ab649998493990020650f2a2c64a719cd
udontur <h@hadrian.cc>
2025-07-17 15:08:22 +0800

    chore: code formatting

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload_file', views.upload_file, name='upload_file'),
+    path("", views.index, name="index"),
+    path("upload_file", views.upload_file, name="upload_file"),
 ]
--- app/views.py
+++ app/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 
 from app.modules import parser
 
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def upload_file(request):
-    if request.method == 'POST':
-        uploaded_file = request.FILES.get('input_file')
+    if request.method == "POST":
+        uploaded_file = request.FILES.get("input_file")
         if uploaded_file:
             raw_latex = parser.pdf_to_raw_latex(uploaded_file)
             parsed_text = parser.parse_raw_latex(raw_latex)
-            return render(request, 'index.html', {'message': 'File processed successfully.', 'parsed_text': parsed_text})
+            return render(
+                request,
+                "index.html",
+                {"message": "File processed successfully.", "parsed_text": parsed_text},
+            )
         else:
-            return render(request, 'index.html', {'error': 'No file uploaded.'})
-    return render(request, 'index.html')
-
+            return render(request, "index.html", {"error": "No file uploaded."})
+    return render(request, "index.html")
